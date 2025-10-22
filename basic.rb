51aae3de206df5745a08ad5409a94fb2713d9650
Burhan dar <burhandar096@gmail.com>
2025-10-22 14:40:26 +0530

    Add files via upload

---
+++ basic.rb
@@ -0,0 +1,15 @@
+#varaibles 
+
+var = 10
+puts var.class
+var = "ruby"
+puts var.class
+
+#..............variable types..........#
+#global varaibles =>$var
+#class varaibles =>@@var
+#instance varaibles =>@var
+#local varaibles =>var
+
+class Example
+end
